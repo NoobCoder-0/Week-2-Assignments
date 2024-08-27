2b476fef3344f7da4f9447a7a53870bbd1a6c103
NoobCoder-0 <sheheryar3847@gmail.com>
2024-08-27 22:56:25 +0500

    Update fileServer.js

--- 02-nodejs/fileServer.js
+++ 02-nodejs/fileServer.js
@@ -1,25 +1,45 @@
-/**
-  You need to create an express HTTP server in Node.js which will handle the logic of a file server.
-  - Use built in Node.js `fs` module
-
-  The expected API endpoints are defined below,
-  1. GET /files - Returns a list of files present in `./files/` directory
-    Response: 200 OK with an array of file names in JSON format.
-    Example: GET http://localhost:3000/files
-
-  2. GET /file/:filename - Returns content of given file by name
-     Description: Use the filename from the request path parameter to read the file from `./files/` directory
-     Response: 200 OK with the file content as the response body if found, or 404 Not Found if not found. Should return `File not found` as text if file is not found
-     Example: GET http://localhost:3000/file/example.txt
-
-    - For any other route not defined in the server return 404
-
-    Testing the server - run `npm run test-fileServer` command in terminal
- */
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
+const port = 3000;
 
+const directoryPath = path.join(__dirname, 'files')
+
+function showFileNames(req, res){
+  
+  fs.readdir(directoryPath, (err, files) =>{
+    if (err){
+      res.status(500).json({error: "Unable to read file names"})
+    }
+          res.json(files)
+})
+  }
+
+function readFileContents(req, res){
+const fileName = req.params.filename;
+const filePath = path.join(directoryPath, fileName)
+
+  fs.readFile(filePath, 'utf8', (err, data) =>{
+    if(err){
+      res.status(404).send("File not found")
+    }
+
+    res.send(data)
+  })
+}
+
+app.get('/files', showFileNames)
+app.get('/file/:filename', readFileContents)
+
+app.use((req,res) =>{
+  res.status(404).send("Route not found")
+})
+
+function started(){
+  console.log(`Example app listening on port${port}`)  
+}
+
+app.listen(port, started);
 
 module.exports = app;
